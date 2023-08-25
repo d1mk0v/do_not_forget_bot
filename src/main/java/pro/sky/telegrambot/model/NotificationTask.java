6822ebb27799c546302eb405e7690b57e7829aca
d1mk0v <79874763588@ya.ru>
2023-08-25 15:53:17 +0500

    2.2

---
+++ src/main/java/pro/sky/telegrambot/model/NotificationTask.java
@@ -0,0 +1,74 @@
+package pro.sky.telegrambot.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+public class NotificationTask {
+
+    @Id
+    private Long id;
+    private Long chatId;
+    private String message;
+    private LocalDateTime date_and_time;
+
+    public NotificationTask() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(Long chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public LocalDateTime getDate_and_time() {
+        return date_and_time;
+    }
+
+    public void setDate_and_time(LocalDateTime date_and_time) {
+        this.date_and_time = date_and_time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NotificationTask that = (NotificationTask) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "NotificationTask{" +
+                "id=" + id +
+                ", chatId=" + chatId +
+                ", message='" + message + '\'' +
+                ", date_and_time=" + date_and_time +
+                '}';
+    }
+}
